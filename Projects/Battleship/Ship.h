b025e61b9867014f5e83fca22a91cd2eca8add38
jrubill <jrubill@g.clemson.edu>
2017-10-12 23:07:58 -0400

    fixed Make on C++

---
+++ Projects/Battleship/Ship.h
@@ -0,0 +1,18 @@
+// Header file for Ship
+
+#ifndef __SHIP_H__
+#define __SHIP_H__
+
+class Ship {
+private:
+  int x;
+  int y;
+
+public:
+  Ship();
+  ~Ship();
+  Ship(const Ship&);
+
+};
+
+#endif //__SQUARE_H__
