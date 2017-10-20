c1ba33402d820a422c496869411d9e62f1020d08
Cavendh <cavendh@g.clemson.edu>
2017-10-19 23:34:07 -0400

    most recent update

--- Projects/BattleSHIP/main.cpp
+++ Projects/BattleSHIP/main.cpp
@@ -26,9 +26,8 @@ void board() {
 for(int i = 0; i < 10; i++) {
   for(int j = 0; j < 10; j++) {
     if(coord1 == i && coord2 == j) {
-      if(coord1 == 3 && coord2 == 5) {
         array1[i-1][j-1] = 3;
-      }
+
       } else {
         array1[i][j] = 0;
       }
