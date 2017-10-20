2b1ce3f1562e6dbc05f163cdd7a8459fc9565eb0
Cavendh <cavendh@g.clemson.edu>
2017-10-20 00:20:09 -0400

    Made a board object

--- Projects/BattleSHIP/main.cpp
+++ Projects/BattleSHIP/main.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <iostream>
 #include "Ship.h"
+#include "Board.h"
 
 #define HIT 1
 #define MISS 2
 #define DESTROYED 3
 #define NEUTRAL 0
 
-int array1[10][10] = {};
 int coord1,coord2;
 
 void Userinput() {
@@ -19,47 +19,13 @@ void Userinput() {
   std::cout << coord2 << std::endl;
 };
 
-void board() {
-
-//array setup
-
-for(int i = 0; i < 10; i++) {
-  for(int j = 0; j < 10; j++) {
-    if(coord1 == i && coord2 == j) {
-        array1[i-1][j-1] = 3;
-
-      } else {
-        array1[i][j] = 0;
-      }
-    }
-  }
-}
-
-
-void printBoard() {
-
-//This Prints the Board
-std::cout << std::endl;
-std::cout <<"  0 1 2 3 4 5 6 7 8 9\n";
-for(int i = 0; i < 10; i++) {
-    std::cout << i << " ";
-  for(int j = 0; j < 10; j++) {
-    std::cout << array1[i][j] << " ";
-  }
-  std::cout << "\n";
-  }
-}
-
-
-
-
-
-
 int main(int argc, char **argv) {
   std::cout << "Make Battleship Work. I dare you" << std::endl;
   Userinput();
-  board();
-  printBoard();
+  Ship ship;
+  Board Board;
+
+
 
   return 0;
 }
